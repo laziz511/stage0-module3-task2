56f8232bcdc1e2a099ed99d8584e42cbeadf3fce
Laziz Djuraev <110926594+laziz511@users.noreply.github.com>
2022-08-19 18:20:45 +0500

    Update JavaVariableNames.java

--- src/main/java/lang/print/gaps/task2/JavaVariableNames.java
+++ src/main/java/lang/print/gaps/task2/JavaVariableNames.java
@@ -2,13 +2,13 @@ package lang.print.gaps.task2;
 
 public class JavaVariableNames {
     public static void main(String[] args) {
-        int zero = 0;
-        int ten = 10;
+        int morrow = 0;
+        int money = 10;
         int hundred = 100;
-        int thousand = 1000;
-        int tenthousand = 10000;
-        int hundredthousand = 100000;
-        int million = 1000000;
-        int onemillion = 1000000;
+        int back = 1000;
+        int underscore = 10000;
+        int sharp = 100000;
+        int star = 1000000;
+        int badExampleOfVarName = 1000000;
     }
 }
